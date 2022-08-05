90b5ceeb39016fa6649d6b12a049433c9e505e2d
AlexeySadykhov <81092836+AlexeySadykhov@users.noreply.github.com>
2022-08-05 15:27:54 +0300

    Add files via upload

---
+++ FM-spectral generator (chord-seqs).py
@@ -0,0 +1,32 @@
+c_line = list(map(float, input('Enter career line frequencies:').split()))
+m_line = list(map(float, input('Enter modulator line frequencies:').split()))
+g_line = list(map(int, input('Enter fm level line values:').split()))
+if not len(c_line) == len(m_line) == len(g_line):
+    print("Error. Lists' length must be the same.")
+    exit(1)
+
+
+def create_spectrum(c, m, gain):
+    array = [c]
+    for i in range(1, gain+1):
+        array.append(c + m * i)
+        array.append(c - m * i)
+    result = [x for x in array if 20 < x < 20000]
+    result.sort()
+    return result
+
+
+def parse_to_lisp(lst):
+    lst1 = ' '.join(str(item) for item in lst)
+    lst2 = lst1.replace('[', '(')
+    lst3 = lst2.replace(']', ')')
+    lst4 = lst3.replace(',', '')
+    return lst4
+
+
+data = [create_spectrum(c_line[i], m_line[i], g_line[i]) for i in range(len(c_line))]
+filename = input('Enter file name to save:')
+file = open(filename + '.txt', 'w')
+file.write(parse_to_lisp(data))
+file.close()
+print('Done')
